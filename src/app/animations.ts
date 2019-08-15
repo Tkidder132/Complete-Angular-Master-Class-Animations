7e8ea9bf42d982302d61ab216dcfd4cf104cbd26
tkidder <no.name@google.com>
2019-08-14 21:46:15 -0500

    bounce out keyframe animation

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -1,4 +1,4 @@
-import { trigger, state, style, transition, animate } from "@angular/core";
+import { trigger, state, style, transition, animate, keyframes } from "@angular/core";
 
 export const fade = trigger('fade', [
     state('void', style({ opacity: 0 })),
@@ -13,6 +13,17 @@ export const slide = trigger('slide', [
         animate(500, style({ transform: 'translateX(-10px)' }))
     ]),
     transition(':leave', [
-        animate('0.5s cubic-bezier(.61, .29, .07, 1.01)', style({ transform: 'translateX(-100%)' }))
+        animate('0.5s ease-out', keyframes([
+            style({ 
+                offset: .2,
+                opacity: 1,
+                transform: 'translateX(20px)'
+            }),
+            style({ 
+                offset: 1,
+                opacity: 0,
+                transform: 'translateX(-100%)'
+            })
+        ]))
     ])
 ]);
